9b1f9d5db6d67294f2b482488332ae23c9d33cd8
gunnarrl <gunnarrl@hawaii.edu>
2025-10-17 14:36:26 -0700

    Create database_test.py

---
+++ backend/database_test.py
@@ -0,0 +1,40 @@
+# backend/database_test.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from backend.database.models import Base
+from backend.main import app, get_db
+from fastapi.testclient import TestClient
+import pytest
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
